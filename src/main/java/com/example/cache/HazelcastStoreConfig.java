143403ff20af17764328827237b1819319605cef
daehyoung <daehyoung@daehyoungui-MacBookAir.local>
2023-02-12 22:42:37 +0900

    tcp ip 를 사용하여 discovering

--- src/main/java/com/example/cache/HazelcastStoreConfig.java
+++ src/main/java/com/example/cache/HazelcastStoreConfig.java
@@ -3,20 +3,19 @@ package com.example.cache;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.config.Config;
+
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class HazelcastStoreConfig {
 
     @Bean
     public HazelcastInstance hazelcastInstance(){
-        Config config = new Config();
-        config.getNetworkConfig().setPort( 5900 );
-        config.setClusterName("device-io-loader");
-        return Hazelcast.newHazelcastInstance(config);
+        return Hazelcast.newHazelcastInstance() ;
     }
 
     @Bean
